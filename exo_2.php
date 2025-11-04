a166bb3a1672c8d447059d3caacaf565a2468ce8
Nicolas Bernard <njr.bernard@gmail.com>
2025-11-04 13:51:44 +0100

    exo2 modifié

--- exo_2.php
+++ exo_2.php
@@ -1,8 +1,10 @@
 <?php
 
 $tableauAges = [1900, 1989, 1950, 2022, 2020, 2003, 1950, 1938, 2014];
-$ageMini = $tableauAges[0];
-$ageMaxi = $tableauAges[0];
+if (count($tableauAges) !=0) {
+    $ageMini = $tableauAges[0];
+    $ageMaxi = $tableauAges[0] ;
+
 $anneePair = 0;
 echo "<u>Avec foreach:</u> <br><br>";
 
@@ -36,4 +38,6 @@ for ($i=0; $i < count($tableauAges); $i++) {
 
 echo "L'année la plus ancienne est: " . "<b>" . $ageMaxi . "</b>" . "<br>";
 echo "L'année la plus récente est: " . "<b>" . $ageMini . "</b>" . "<br><br>";
-echo "Le nombre d'années pairs est: " . "<b>" . $anneePair . "</b>" . "<br><br>";
+echo "Le nombre d'années pairs est: " . "<b>" . $anneePair . "</b>" . "<br><br>";}
+
+else {echo "Tableau vide";}
