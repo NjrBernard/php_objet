c0e424f6725c6a81c222b879e1cf62819b62d7f2
Nicolas Bernard <njr.bernard@gmail.com>
2025-11-04 12:17:40 +0100

    exo2

---
+++ exo2.php
@@ -0,0 +1,39 @@
+<?php
+
+$tableauAges = [1900, 1989, 1950, 2022, 2020, 2003, 1950, 1938, 2014];
+$ageMini = $tableauAges[0];
+$ageMaxi = $tableauAges[0];
+$anneePair = 0;
+echo "<u>Avec foreach:</u> <br><br>";
+
+foreach ($tableauAges as $age) {
+    if ($age > $ageMaxi) {
+        $ageMaxi = $age;
+    }
+    if ($age < $ageMini) {
+        $ageMini = $age;
+    }
+    if ($age % 2 == 0) {
+        $anneePair++;
+    }
+}
+
+    echo "L'année la plus ancienne est: " . "<b>" . $ageMaxi . "</b>" . "<br>";
+    echo "L'année la plus récente est: " . "<b>" . $ageMini . "</b>" . "<br><br>";
+    echo "Le nombre d'années pairs est: " . "<b>" . $anneePair . "</b>" . "<br><br>";
+
+echo "<u>Avec la boucle for:</u> <br><br>";
+
+for ($i=0; $i < count($tableauAges); $i++) {
+    if ($tableauAges[$i] > $ageMaxi) {
+        $ageMaxi = $tableauAges[$i];
+    }
+    if ($tableauAges[$i] < $ageMini) {
+        $ageMini = $tableauAges[$i];
+    }
+
+}
+
+echo "L'année la plus ancienne est: " . "<b>" . $ageMaxi . "</b>" . "<br>";
+echo "L'année la plus récente est: " . "<b>" . $ageMini . "</b>" . "<br><br>";
+echo "Le nombre d'années pairs est: " . "<b>" . $anneePair . "</b>" . "<br><br>";
