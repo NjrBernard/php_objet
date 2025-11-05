50c1c566e645cdeab6409f1b99f29cee3f052287
Nicolas Bernard <njr.bernard@gmail.com>
2025-11-05 10:46:12 +0100

    Jusqu'à exo 5

--- country.php
+++ country.php
@@ -13,59 +13,105 @@ class country {
         $this->continent = $continent;
     }
 
-    public function getInfo() {
-        echo "Le " . $this->name . " a pour capitale " . $this->capital . ". Sa population est de  " . $this->population . " millions d'habitants. Il se situe sur le continent: " .  $this->continent;
+    public function getInfo() :void {
+        echo "Le " . $this->name . " a pour capitale " . $this->capital . ". Sa population est de  " . $this->population . " millions d'habitants. Il se situe sur le continent: " .  $this->continent ;
     }
 
-    public function afficheName() {
-        echo $this->name;
+    //Mes getters
+    public function getName() {
+        return $this->name;
+    }
+    public function getCapital() {
+        return $this->capital;
+    }
+    public function getPopulation() {
+        return $this->population;
+    }
+    public function getContinent() {
+        return $this->continent;
     }
 
-    public function afficheCapital() {
-        echo $this->capital;
+//Fin des getters
+
+// Mes setters
+
+    public function modifName($nouveauNom) {
+        return $this->name = $nouveauNom;
+    }
+    public function modifCapital($nouvelleCapital) {
+        return $this->capital = $nouvelleCapital;
     }
 
-    public function modifPop() { /* Augmente de 10 la population*/
-        echo "Le " . $this->name . " a pour capitale " . $this->capital . ". Sa population est de  " . ($this->population + 10) . " millions d'habitants. Il se situe sur le continent: " .  $this->continent;
+    public function modifPop($nouvellePop) { /* Augmente de 10 la population*/
+        return $this->population = $nouvellePop;
+        /*echo "Le " . $this->name . " a pour capitale " . $this->capital . ". Sa population est de  " . ($this->population + 10) . " millions d'habitants. Il se situe sur le continent: " .  $this->continent . ".";*/
     }
 
-    public function afficheContinent()
+    public function afficheContinent() :void
     {
         switch ($this->continent) {
             case "Europe":
-                $this->continent = "européen";
+                echo "Le " . $this->name . " a pour capitale " . $this->capital . ". Sa population est de  " . $this->population . " millions d'habitants. Il se situe sur le continent européen";
+                //$this->continent = "européen";
                 break;
             case "Amérique du Nord":
-                $this->continent = "nord-américain";
+                echo "Le " . $this->name . " a pour capitale " . $this->capital . ". Sa population est de  " . $this->population . " millions d'habitants. Il se situe sur le continent nord-américain";
+                //$this->continent = "nord-américain";
                 break;
             case "Amérique du Sud":
-                $this->continent = "sud-américain";
+                echo "Le " . $this->name . " a pour capitale " . $this->capital . ". Sa population est de  " . $this->population . " millions d'habitants. Il se situe sur le continent sud-américain";
+                //$this->continent = "sud-américain";
                 break;
             case "Asie":
-                $this->continent = "asiatique";
+                echo "Le " . $this->name . " a pour capitale " . $this->capital . ". Sa population est de  " . $this->population . " millions d'habitants. Il se situe sur le continent asiatique";
+                //$this->continent = "asiatique";
                 break;
             case "Afrique":
-                $this->continent = "africain";
+                echo "Le " . $this->name . " a pour capitale " . $this->capital . ". Sa population est de  " . $this->population . " millions d'habitants. Il se situe sur le continent africain";
+                //$this->continent = "africain";
                 break;
         }
 
-        echo "Le " . $this->name . " a pour capitale " . $this->capital . ". Sa population est de  " . ($this->population + 10) . " millions d'habitants. Il se situe sur le continent " . $this->continent;
 
     }
 
-
+//Fin des setters
 
 }
+
+class DevelopedCountry extends country {
+    public $gdp;
+
+    public function __construct($name, $capital, $population, $continent, $gdp) {
+        parent::__construct($name, $capital, $population, $continent);
+        $this->gdp = $gdp;
+    }
+
+    public function getGdp() : void{
+        parent::getInfo();
+        echo " Son PIB est de " . $this->gdp . " Mds€";
+    }
+
+}
+
+
 
 echo "<u>Exercice 3</u><br><br>";
 $pays1 = new country("France", "Paris", 70, "Europe");
 echo "   <i>   Affichage sans modification</i><br>";
 $pays1->getInfo(); echo "<br><br>";
-echo "   <i>   Affichage avec + 10 millions d'habitants grâce à la fonction modifPop()</i><br>";
-$pays1->modifPop(); echo "<br><br>";
+echo "   <i>   Affichage avec modifPop()</i>";
+$pays1->modifPop(541); echo "<br><br>";
+$pays1->getInfo(); echo "<br><br>";
 echo "   <i>   Affichage avec transformation de la syntaxe pour l'écriture du continent</i><br>";
 $pays1->afficheContinent();echo "<br><br>";
 
 
 echo "<br>";
 echo "<u>Exercice 2</u><br><br>";
+
+$pays2 = new DevelopedCountry("Allemagne", "Berlin", 80, "Europe", 4300);
+
+$pays2->getInfo(); echo "<br><br>";
+echo "<i> Affichage avec le PIB </i><br>";
+$pays2->getGdp(); echo "<br><br>";
--- test_country.php
+++ test_country.php
@@ -1,11 +1,47 @@
 <?php
 require_once "country.php";
 
-$pays1 = new Country("Italie", "Rome", 50, "Europe");
-$pays1->getInfo();
-echo "<br>";
 
-$pays2 = new Country("Etats-Unis", "Washington D.C.", 400, "Amérique du Nord");
-$pays2->getInfo();
 
-$tableau = [$pays1, $pays2] ;
+$Italie = new Country("Italie", "Rome", 50, "Europe");
+$Italie->getInfo();
+echo "<br><br>";
+
+$EtatsUnis = new Country("Etats-Unis", "Washington D.C.", 400, "Amérique du Nord");
+$EtatsUnis->getInfo(); echo "<br><br>";
+
+$Vietnam = new Country("Vietnam", "Hanoi", 120, "Asie");
+$Vietnam->afficheContinent();echo "<br><br>";
+
+$Perou = new Country("Pérou", "Lima", 50, "Amérique du Sud");
+$Perou->afficheContinent();echo "<br><br>";
+
+$Suede = new Country("Suede", "Stockholm", 40, "Europe");
+$Suede -> afficheContinent();echo "<br><br>";
+
+$Kenya = new Country("Kenya", "Nairobi", 30, "Afrique");
+$Kenya->afficheContinent();echo "<br><br>";
+
+$tableau = [$Vietnam, $Italie, $EtatsUnis, $Perou, $Suede, $Kenya];
+$i=1;
+echo "<u>Exercice 4</u><br><br>";
+echo "<i>Tableau des pays</i>";
+echo "<table>
+<tr>
+<th>Pays</th>
+<th>Capitale</th>
+<th>Population</th>
+<th>Continent</th>
+</tr>";
+
+foreach ($tableau as $country) {
+
+    echo "<tr><td>" . "Pays " . $i . ": " . "</td><td>" . $country->name . "</td><td>" . $country->population . "</td><td>" . $country->continent . "</td></tr><br>";
+    $i++;
+}
+echo "</table>";
+
+echo "<br><br>";
+
+
+
